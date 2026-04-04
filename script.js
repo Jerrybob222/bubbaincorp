93664093f9792173b2dc9db3853c342281eca117
Coolhubert <1backup5acc@gmail.com>
2026-04-03 22:59:20 -0500

    TEST 3

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ function toggleNav(){
     }
 
 }
+
+
+
+
+
+// Function used by navbar button
+function cloak() {
+    document.title = "New Tab";
+    let icon = document.querySelector("link[rel~='icon']");
+    if (icon) icon.href = "/images/psicon.png";
+}
+
+// Add event listener after navbar is loaded
+function initNavbar() {
+    const navButton = document.querySelector(".nav-button");
+    if (navButton) {
+        navButton.addEventListener("click", cloak);
+    }
+}
+
+// Load navbar dynamically
+fetch("/navbar.html")
+    .then(r => r.text())
+    .then(data => {
+        document.getElementById("navbar-container").innerHTML = data;
+        initNavbar(); // set up button listeners
+    })
+    .catch(err => console.error("Navbar failed to load:", err));
